c4890b8943a01651b49d56b77692e27f5a313ee9
Benjamin Herson <benjaminherson@gmail.com>
2017-03-08 15:46:06 -0800

    add bootstrap to project, add basic navBar, add icon/links to linkedin and github profiles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Nav, NavItem } from 'react-bootstrap';
+import github from '../assets/github.png';
+import linkedin from '../assets/linkedin.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Nav bsStyle="pills">
+            <NavItem>Projects</NavItem>
+            <NavItem>Shark Page</NavItem>
+            <a className="navLink inIcon" href="https://www.linkedin.com/in/benjaminherson/" target="_blank"><img src={linkedin} alt={"link to linkedin"}/></a>
+            <a className="navLink" href="https://github.com/rallyb13" target="_blank"><img src={github} alt={"link to github"}/></a>
+        </Nav>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
