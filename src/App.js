614c761f17e17e936f4fd7de5e418a2d9d1bd82b
Benjamin Herson <benjaminherson@gmail.com>
2017-03-09 09:37:18 -0800

    add fontAwesome, remove non-matched icons and replace with font-awesome, add basic navbar styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
-import github from '../assets/github.png';
-import linkedin from '../assets/linkedin.png';
+import FontAwesome from 'react-fontawesome';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav bsStyle="pills">
+        <Nav className="navigation" bsStyle="pills">
             <NavItem>Projects</NavItem>
             <NavItem>Shark Page</NavItem>
-            <a className="navLink inIcon" href="https://www.linkedin.com/in/benjaminherson/" target="_blank"><img src={linkedin} alt={"link to linkedin"}/></a>
-            <a className="navLink" href="https://github.com/rallyb13" target="_blank"><img src={github} alt={"link to github"}/></a>
+            <a className="navLink" href="https://www.linkedin.com/in/benjaminherson/" target="_blank">
+              <FontAwesome name="linkedin-square" size="3x" />
+            </a>
+            <a className="navLink" href="https://github.com/rallyb13" target="_blank">
+              <FontAwesome name="github-square" size="3x" />
+            </a>
         </Nav>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
