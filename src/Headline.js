41e0fa34efc8ff1d7481bc8502281fa8240dedec
Benjamin Herson <benjaminherson@gmail.com>
2017-03-24 09:16:41 -0700

    fix typos and readability issues with content

--- src/Headline.js
+++ src/Headline.js
@@ -7,7 +7,7 @@ class Headline extends Component {
       <div className="headline">
         <img className="loftyFace" src={LoftyFace} alt={"Me with a book."} />
         <h1>Benjamin D. Herson</h1>
-        <h4>Front-End Developer, Writer, Poet, & Game-Maker</h4>
+        <h4>Front-End Developer, Writer, Poet & Game-Maker</h4>
       </div>
     )
   }
--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ const projectData = [
     img: "Quid.png",
     alt: "Quid, a match three game",
     caption: "My major side project for 2016 was a match-three game about political corruption. ",
-    more: "Originally, I only wanted to test out my ability to make a fully-playable game. Doing such a large project with my own specific goals allowed me to not only amp up my JS skills, but see the whole development process. I kept a trello board and broke everything into tickets, even deciding a few ideas could sit out for MVP. The biggest highlight was the back-to-back success of making recursive matching (you place a token where it makes a match and the newly created token also makes a match) work then then the wild card (megaphone token). As a guy who always works best with a team, though, I couldn't leave my friends out and brought them over for mini-hackithons, occasionally acting more as project manager. I plan to rebuild the game in C# using Unity with a small game design team I've formed."
+    more: "Originally, I only wanted to test out my ability to make a fully-playable game. Doing such a large project with my own specific goals allowed me to not only amp up my JS skills, but see the whole development process. I kept a Trello board and broke everything into tickets, even holding off on some ideas to get to an Minimum Viable Product. The biggest highlight was the back-to-back success of making recursive matching (you place a token where it makes a match and the newly created token also makes a match) work and then the wild card (megaphone token). As a guy who always works best with a team, though, I couldn't leave my friends out and brought them over for mini-hackathons, occasionally acting more as project manager. I plan to rebuild the game in C# using Unity with a small game design team I've formed."
   },
   {
     name: "Text Adventure",
@@ -18,7 +18,7 @@ const projectData = [
     img: "TextAdv.png",
     alt: "Opening screen for a text adventure game",
     caption: "I'm learning Unity and C#! This is from one of the tutorials. ",
-    more: "Up on gamebucket for only a month, this is the latest game (that I'm proud of), a simple text-based game. The nice thing about a tutorial slow enough for people just learning to code is that I can apply a dash of more advanced coding logic to even these early games (if you make it into the forest, see what happens when you keep choosing R to Run)."
+    more: "Up on GameBucket for only a month, this is the latest game (that I'm proud of), a simple text-based game. The nice thing about a tutorial slow enough for people just learning to code is that I can apply a dash of more advanced coding logic to even these early games (if you make it into the forest, see what happens when you keep choosing R to Run)."
   },
   {
     name: "Chess",
@@ -27,7 +27,7 @@ const projectData = [
     img: "Chess.png",
     alt: "Chess, built in week 5 of Epicodus",
     caption: "This group project at Epicodus was my first Epic Win as a developer. ",
-    more: "The assignment was to build one of the smaller, suggested projects across a full week as a pair coding project. Wanting to get more experience with git (and working with other people's code), I formed a 4-person team and as two pairs we built Go Fish, which was too easy. So on Wednesday I formed a new team and we went even bigger: we'd build Chess. We white-boarded as a team and changed pairs every day. Soon we had rooks that could move. Then we had bishops too, and pieces got captured. The pawns went up last (satisfying moment of logic: for determining the validity of a pawn's move, you have to check for an opponent's presence first). Our success was so EPIC that it forever changed how the Epicodus curriculum as group projects became the conclusion to each unit. This is the game that started it all."
+    more: "The assignment was to build one of the smaller, suggested projects across a full week as a pair coding project. Wanting to get more experience with git (and working with other people's code), I formed a 4-person team and as two pairs we built Go Fish, which was too easy. So on Wednesday I formed a new team and we went even bigger: we'd build a chess game. We white-boarded as a team and changed pairs every day. Soon we had rooks that could move. Then we had bishops too, and pieces got captured. The pawns went up last (satisfying moment of logic: for determining the validity of a pawn's move, you have to check for an opponent's presence first). Our success was so EPIC that it forever changed the Epicodus curriculum, inspiriing the instructors to make group projects the conclusion of each course unit. This is the game that started it all."
   }
 ];
 
