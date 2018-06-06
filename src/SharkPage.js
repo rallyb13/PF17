f4a6542730e32d34c4eeffa710570f8d187e537b
Benjamin Herson <benjaminherson@gmail.com>
2018-06-05 21:12:53 -0700

    add recent events to shark, fix navigation padding

--- src/SharkPage.js
+++ src/SharkPage.js
@@ -47,13 +47,12 @@ class SharkPage extends Component {
             <p className="convo">Jeff: <em>It's a road trip with a mission to correct typos in signs!</em></p>
             <p className="convo" style={{marginBottom:"10px"}}>Me: <em>...I like road trips.</em></p>
             <p>
-              Our goofy adventure attracted a little media attention, then a lot. Fortunately, we were more ready than most when the "fame = publishable" equation helped us get a book deal. After honing our skills for over a decade, we could actually write. And so we created <a target="_blank" href={bookLink}>The Great Typo Hunt</a>!
+              Fortunately, we were more ready than most when, our goofy adventure having attracted some media attention, the "fame = publishable" equation helped us get a book deal. After honing our skills for over a decade, we could actually write. And so we created <a target="_blank" href={bookLink}>The Great Typo Hunt</a>!
               With a 36-city book tour, we sold through our hardback print run, and I expected the paperback to do even better (bookseller insight). Unfortunately, by the paperback release, Borders had been liquidated.
             </p>
             <p>
-              Worse, after seeing how the sausage was made, I recognized that the business side of publishing didn't suit me. I didn't want to be self-marketing all the time; I wanted to be writing.
-              I'd accomplished my childhood dream, yet I somehow hadn't gotten what I'd really wanted.
-              So what did I want?
+              After seeing how the sausage was made, I recognized that the business side of publishing didn't suit me. I didn't want to be self-marketing all the time; I wanted to be writing.
+              I'd accomplished my childhood dream (Hooray!), but, in the words of Jed Bartlet, "What's Next?"
             </p>
           </div>
           
@@ -64,32 +63,33 @@ class SharkPage extends Component {
               <p>photos by Grace Mekarski</p>
             </div>
             <p>
-              Books began pointing me in a new direction. <a target="_blank" href="https://www.amazon.com/Reality-Broken-Games-Better-Change/dp/0143120611">Reality Is Broken by Jane McGonigal</a> really
+              Ever the reader, I took my cue for a new direction from some good books. <a target="_blank" href="https://www.amazon.com/Reality-Broken-Games-Better-Change/dp/0143120611">Reality Is Broken by Jane McGonigal</a> really
               spoke to me, and <a target="_blank" href="https://www.amazon.com/Program-Be-Programmed-Commands-Digital/dp/159376426X">Douglas Rushkoff's Program Or Be Programmed</a> made a great argument.
               Plus, I'd already been a fan of <a target="_blank" href="https://www.amazon.com/Age-Spiritual-Machines-Computers-Intelligence/dp/0140282025">Ray Kurzweil's Age of Spiritual Machines</a> and its
-              follow-up <a target="_blank" href="https://www.amazon.com/Singularity-Near-Humans-Transcend-Biology/dp/0143037889">The Singularity Is Near</a>. (Can you tell I was once a bookseller?)
-              Meanwhile, my partner was having an absolute blast in her first years as a developer. This cannot be understated.
+              follow-up <a target="_blank" href="https://www.amazon.com/Singularity-Near-Humans-Transcend-Biology/dp/0143037889">The Singularity Is Near</a>.
+              Oh yeah, and my partner was having an absolute blast in her first years as a developer.
             </p>
-            <div className="zebra">
-              <img src={Zebra} alt="Zebra Head"/>
-              <h6>I'm on <a target="_blank" href="https://medium.com/@sexandstartups/zebrasfix-c467e55f9d96#.2nw798vnf">Team Zebra</a>.</h6>
-            </div>
             <p onMouseOver={this.changeCounter.bind(this)}>
-              I finally decided to become 21st-century literate when I realized my storytelling drive could be satisfied by game-making.
               In 2014, while working at Powell's, I started learning to code, and before the year was out, I'd signed up for <a target="_blank" href="https://www.learnhowtoprogram.com">Epicodus</a> to 
               accelerate the process. At Epicodus, we coded in pairs, and I even found a team to help me build bigger projects than we'd been assigned. Our group project experiment
               became such an Epic Win that we fundamentally changed the curriculum for all future Epicodus students.
             </p>
             <p>
-              Epicodus concluded with an internship at Thetus, which hired me for the same reason a PM there gave me the moniker "Benjamin BugCrusher," my tenacity.
-              On Thursday, October 8th of 2015, I had my single best coding day ever, and I knew I'd made the right choice.
-              I amped up my JavaScript skills quickly thanks to the great team I had at Thetus (team name: <a target="_blank" href="https://en.wikipedia.org/wiki/Clurichaun">Clurichauns</a>). At Nike, I landed on the
+              Epicodus concluded with an internship at Thetus, which was such a good fit, they hired me. A PM there gave me the moniker "Benjamin BugCrusher" for my tenacity.
+              On Thursday, October 8th of 2015, I worked through the logic of rendering the a complex navigation bar, and had so much fun with it that I knew I'd made the right choice.
+              I amped up my JavaScript skills quickly thanks to the great team I had at Thetus (team name: <a target="_blank" href="https://en.wikipedia.org/wiki/Clurichaun">Clurichauns</a>). When Thetus disappeared out from under us, I leapt into a role at Nike. Somehow landing on the
               team responsible for one of the largest eCommerce sites in the world, adding analytics to their B2B applications.
             </p>
+            <div className="zebra">
+              <img src={Zebra} alt="Zebra Head"/>
+              <h6>I'm on <a target="_blank" href="https://medium.com/@sexandstartups/zebrasfix-c467e55f9d96#.2nw798vnf">Team Zebra</a>.</h6>
+            </div>
             <p>
-              After completing my contract with Nike, I joined HealthSparq because I believe in their mission. As I've honed in on amping up my skills as a JavaScript developer, I've also deepened my understanding of what makes a good user experience. (It's funny how that hopped off the computer screen and became a lens for viewing everything.)
-              HealthSparq also offered me the opportunity to return to a leadership role (for the first time since becoming a developer) as I stepped into the role as Scrum Master.
+              After completing my contract with Nike, I joined HealthSparq, introducing myself by turning their test suite green. My emphasis on quality (BugCrusher, remember?) eventually got me placed onto a team handling the highest priority issues. The new team got off to a bumpy start, so I stepped up and became the Scrum Master (though no stranger to leadership roles, this was my first since learning to code).
             </p>
+            <p>I'd planned to stay longer at HealthSparq, but a friend clued me into an opportunity I simply couldn't pass up, working as part of a much smaller front-end team at <span title="Someone finally said: Gee, maybe relentlessly spamming everyone ain't a great way to build trust in your brand, kids."> Lytics, a start-up that actually <em>applies</em> data analytics to marketing</span>. With more data visualization work and opportunities to dig into data science, I've got a lot more to learn, which is just how I like it. Plus, the smaller team means I get to do more of the high visibility work, see my code in production.</p>
+            <p>Meanwhile, I'm co-organizing a couple MeetUps. In summer 2017, I got together with two fellow Epicodus grads to found an Epicodus Alumni Meetup. Somehow I also ended up stepping in as an additional co-organizer for JavaScript Admirers, which has long been one of my favorite MeetUps in town. I regularely attend the React Meetup, irregularly attend Node, and pop up at some others. I guess you could say I've gone All In on this tech lifestyle. There's a simple reason for that.</p>
+            <p>I found my people. In DC it was Poetry Slam and booksellers, but in Portland, it's been tech folks, my Epicoders in particular. Challenge-loving, creative thinkers who aren't ashamed to nerd out, can talk with you for hours about nothing or everything, and share a vision for the positive potential of tech. And what are we up to next? Well, this story is only beginning...</p>
           </div>
           <SharkImg count={this.state.count} left={true} src={"SharkToRight.png"}/>
           <SharkImg count={this.state.count + 7} left={false} src={"SharkToLeft.png"}/>
