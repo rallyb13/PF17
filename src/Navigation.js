17becca75730167820a17cf8592b6a24dd7a5575
Benjamin Herson <benjaminherson@gmail.com>
2018-06-05 22:08:36 -0700

    update fontAwesome, add downloadable resume

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import FontAwesome from 'react-fontawesome';
+import BDH_Resume from '../public/BDH_Resume.pdf';
 
 class Navigation extends Component {
   render() {
@@ -8,10 +9,13 @@ class Navigation extends Component {
       <div className="navigation">
           <Link to="/" className="navPage"><h4>Projects</h4></Link>
           <Link to="/shark" className="navPage"><h4>Shark Page</h4></Link>
-          <a className="navLink" href="https://www.linkedin.com/in/benjaminherson/" target="_blank">
+          <a className="navLink pdfIcon" title="Benjamin's Resume" href={BDH_Resume} download target="_blank" rel="noopener">
+            <FontAwesome name="file-pdf-o" size="3x"/>
+          </a>
+          <a className="navLink" href="https://www.linkedin.com/in/benjaminherson/" target="_blank" rel="noopener">
             <FontAwesome name="linkedin-square" size="3x" />
           </a>
-          <a className="navLink" href="https://github.com/rallyb13" target="_blank">
+          <a className="navLink" href="https://github.com/rallyb13" target="_blank" rel="noopener">
             <FontAwesome name="github-square" size="3x" />
           </a>
       </div>
